aa7ece1e51808071ade662e98e25c70d7f8fa758
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-05-23 18:45:16 +0400

    feat: add section works

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,14 @@
 import About from "./about/About"
 import Expertise from "./expertise/Expertise"
+import Works from "./works/Works"
+
 
 const Main = () => {
     return (
         <main>
             <About />
             <Expertise />
+            <Works />
         </main>
     )
 }
