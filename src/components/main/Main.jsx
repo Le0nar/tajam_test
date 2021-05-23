28578d342af6fbe09d2424aa68210405007bd1c3
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-05-23 16:49:41 +0400

    feat: add section expertise

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,11 @@
 import About from "./about/About"
+import Expertise from "./expertise/Expertise"
 
 const Main = () => {
     return (
         <main>
             <About />
+            <Expertise />
         </main>
     )
 }
