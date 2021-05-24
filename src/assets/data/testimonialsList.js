4ba3af3662f128407385db2748c680c7036f00d3
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-05-24 12:44:47 +0400

    feat: add section testimonials

---
+++ src/assets/data/testimonialsList.js
@@ -0,0 +1,45 @@
+import reviewer1 from "../reviewers/reviewer1.png";
+import reviewer2 from "../reviewers/reviewer2.png";
+import reviewer3 from "../reviewers/reviewer3.png";
+import reviewer4 from "../reviewers/reviewer4.png";
+import reviewer5 from "../reviewers/reviewer5.png";
+
+export const reviewers = {
+  testimonials: [
+    {
+      name: "SEMF UCUK",
+      prof: "FOUNDER",
+      photo: reviewer1,
+      review:
+        "This is first Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.",
+    },
+    {
+      name: "DIK ADALIN",
+      prof: "ENGINEERING",
+      photo: reviewer2,
+      review:
+        "This is second Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.",
+    },
+    {
+      name: "JANE GALADRIEL",
+      prof: "CEO TENGKUREP",
+      photo: reviewer3,
+      review:
+        "This is third Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.",
+    },
+    {
+      name: "JENG KOL",
+      prof: "DESIGNER",
+      photo: reviewer4,
+      review:
+        "This is fourth Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.",
+    },
+    {
+      name: "PET ROMAK",
+      prof: "MARKETING",
+      photo: reviewer5,
+      review:
+        "This is fifth Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.",
+    },
+  ],
+};
---
+++ src/components/main/testimonials/Testimonials.jsx
@@ -0,0 +1,52 @@
+import "./testimonials.scss";
+import quotes from "../../../assets/icons/quotes.png";
+import { useState } from "react";
+import { reviewers } from "../../../assets/data/testimonialsList";
+
+const Testimonials = () => {
+  const [currentReviewNumber, setCurrentReviewNumber] = useState(2);
+  const reviews = reviewers.testimonials;
+  return (
+    <section className="testimonials">
+      <div className="container testimonials__container">
+        <div>
+          <img src={quotes} alt="quotes" className="testimonials__quotes" />
+        </div>
+        <p className="testimonials__review col-md-10">
+          {reviews[currentReviewNumber].review}
+        </p>
+        <div className="testimonials__author">
+          <span className="testimonials__author__name">
+            {reviews[currentReviewNumber].name}
+          </span>
+          <span className="testimonials__author__prof">
+            {reviews[currentReviewNumber].prof}
+          </span>
+        </div>
+        <div className="testimonials__photos col-md-4">
+          {reviews.map((el, i) =>
+            i === currentReviewNumber ? (
+              <img
+                key={el.name}
+                src={el.photo}
+                alt="reviewer"
+                className="testimonials__photos__active"
+                onClick={() => setCurrentReviewNumber(i)}
+              />
+            ) : (
+              <img
+                key={el.name}
+                src={el.photo}
+                alt="reviewer"
+                className="testimonials__photos__inactive"
+                onClick={() => setCurrentReviewNumber(i)}
+              />
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
